446f211dcc56cbec23430bd4b694956d33e74d8f
Juan Torres <jdtorreserazo@gmail.com>
2024-10-16 09:03:59 -0700

    Resume Done, Section title Done

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -48,32 +48,28 @@ const projects = [
 
 const Projects = () => {
     return (
-        <section id="projects" className="py-20">
-            <div className="container mx-auto px-4">
-                <h2 className="text-4xl font-bold text-center mb-12">Projects</h2>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                    {projects.map((project, index) => (
-                        <div key={index} className="border rounded-lg shadow-md p-6">
-                            <h3 className="text-2xl font-semibold">{project.name}</h3>
-                            <p className="mt-2 text-gray-600">{project.stack}</p>
-                            <div className="mt-4">
-                                <a
-                                    href={project.codeLink}
-                                    className="text-blue-600 hover:underline">
-                                    Get Code
-                                </a>{" "}
-                                |
-                                <a
-                                    href={project.liveLink}
-                                    className="ml-2 text-blue-600 hover:underline">
-                                    Visit Site
-                                </a>
-                            </div>
-                        </div>
-                    ))}
+      <section id="projects" className="pt-28 k2d text-white/95">
+        <div className="container mx-auto px-4">
+          <h2 className="sectionTitle text-4xl font-bold text-center mb-12">Projects</h2>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            {projects.map((project, index) => (
+              <div key={index} className="border rounded-lg shadow-md p-6">
+                <h3 className="text-2xl font-semibold">{project.name}</h3>
+                <p className="mt-2 text-gray-600">{project.stack}</p>
+                <div className="mt-4">
+                  <a href={project.codeLink} className="text-blue-600 hover:underline">
+                    Get Code
+                  </a>{" "}
+                  |
+                  <a href={project.liveLink} className="ml-2 text-blue-600 hover:underline">
+                    Visit Site
+                  </a>
                 </div>
-            </div>
-        </section>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
     );
 };
 
