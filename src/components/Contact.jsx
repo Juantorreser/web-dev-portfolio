60adcbad9c8512ec82804975dd6fa9d9e0a97fe1
Juan Torres <jdtorreserazo@gmail.com>
2024-10-18 20:00:42 -0700

    phone Number field fixed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -88,7 +88,7 @@ const Contact = () => {
               <div className="relative z-0 w-full mb-5 ">
                 <input
                   type="tel"
-                  pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                  pattern="[0-9]{3}[0-9]{3}[0-9]{4}"
                   name="phone"
                   id="phone"
                   className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-pri/50 dark:focus:border-pri focus:outline-none focus:ring-0 focus:border-blue-600 peer"
@@ -99,7 +99,7 @@ const Contact = () => {
                 <label
                   htmlFor="phone"
                   className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-85 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-pri peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-85 peer-focus:-translate-y-6">
-                  Phone number (123-456-7890)
+                  Phone number (1234567890)
                 </label>
               </div>
               <div className="relative z-0 w-full mb-5 lg:mb-0 ">
