bc1b8e9241895a167f18678dc2479eae7f85de8e
Juan Torres <jdtorreserazo@gmail.com>
2024-10-18 19:46:58 -0700

    Fixed Sizes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,32 +22,32 @@ const Hero = () => {
   };
   return (
     <section id="hero" className=" h-svh flex items-center justify-center flex-col w-full">
-      <div className="text-center k2d h-[80%] gap-2 lg:gap-10 flex justify-center items-center flex-col-reverse lg:flex-row">
+      <div className="text-center k2d h-[80%] gap-6 lg:gap-10 flex justify-end md:justify-center items-center flex-col-reverse lg:flex-row">
         <motion.div
           initial="hidden"
           animate="visible"
           variants={containerVariants}
           className="left text-white">
           <motion.h1
-            className="text-sm md:text-lg xl:text-2xl opacity-70 font-light"
+            className="text-lg xl:text-2xl opacity-70 font-light"
             variants={itemVariants}>
             Hi, my name is
           </motion.h1>
 
           <motion.h2
-            className="text-2xl md:text-4xl lg:text-5xl xl:text-7xl  lg:mt-2 font-semibold"
+            className="text-4xl lg:text-6xl xl:text-7xl mt-2 font-semibold"
             variants={itemVariants}>
             JD TORRES
           </motion.h2>
 
           <motion.h3
-            className="text-lg md:text-xl lg:text-3xl xl:text-6xl lg:mt-2 text-border font-black"
+            className="text-xl lg:text-4xl xl:text-6xl mt-2 text-border font-black"
             variants={itemVariants}>
             Frontend Developer
           </motion.h3>
 
           <motion.button
-            className="mt-4 lg:mt-7 px-6 py-3 bg-pri rounded-lg shadow-md hover:opacity-75 transition"
+            className="mt-6 px-6 py-3 bg-pri rounded-lg shadow-md hover:opacity-75 transition"
             whileHover={{scale: 1.05}} // Adds hover animation
             whileTap={{scale: 0.95}} // Adds tap animation
             variants={itemVariants}>
@@ -58,7 +58,7 @@ const Hero = () => {
           <img
             src="/hero.png"
             alt="Hero Image"
-            className="heroImage w-[220px] md:w-[260px] lg:w-[350px] xl:w-[650px]"
+            className="heroImage w-[220px] md:w-[260px] lg:w-[350px] xl:w-[650px] mt-10 md:mt-0"
           />
         </div>
       </div>
